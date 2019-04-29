8b1c3b55da70ef18c8412e9e50ee0ac0c50bbf1f
brandonzink <brandon.zink@colorado.edu>
2019-04-29 13:21:15 -0600

    Not working any better.

--- Pitch_Selection_Neural_Network.py
+++ Pitch_Selection_Neural_Network.py
@@ -14,7 +14,6 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Helper libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import sys
@@ -123,11 +122,11 @@ def get_pitcher_model(first_name, last_name):
 		test_data['on_1b'] = test_data['on_1b'].fillna(value=0).astype(bool).astype(int)
 
 		#Get the data we need and drop any null values (which is why it double selects)
-		train_data_input = train_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number', 'pitch_code']].dropna()
+		train_data_input = train_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_1', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number', 'pitch_code']].dropna()
 		train_data_result = train_data_input[['pitch_code']]
-		train_data_input = train_data_input[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']]
+		train_data_input = train_data_input[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_1', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']]
 
-		test_data = test_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number', 'pitch_code']].dropna()
+		test_data = test_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_1', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number', 'pitch_code']].dropna()
 
 		return train_data_input, train_data_result, test_data
 
@@ -138,31 +137,34 @@ def get_pitcher_model(first_name, last_name):
 		label_encoder_handedness = LabelEncoder()
 		train_data['stand'] = label_encoder_handedness.fit_transform(train_data['stand'])
 		test_data['stand'] = label_encoder_handedness.fit_transform(test_data['stand'])
-
 		sc = StandardScaler()
 		train_data = sc.fit_transform(train_data)
-		test_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']] = sc.fit_transform(test_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']])
+		test_data_result = test_data[['pitch_code']]
+		test_data = test_data = test_data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_1', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']]
+		test_data = sc.fit_transform(test_data)
 
 		max_pitch_val = max(train_data_result['pitch_code'])
 
 		train_data_result['pitch_code'] = train_data_result['pitch_code']/max_pitch_val
-		test_data['pitch_code'] = test_data['pitch_code']/max_pitch_val
+		test_data_result = test_data_result/max_pitch_val
 
-		return train_data, train_data_result, test_data, max_pitch_val
+		return train_data, train_data_result, test_data, test_data_result, max_pitch_val
 
 	def create_model(train_data_input, train_data_result, saved_model_name):
+		
 		#Initialize nerual network
 		model = Sequential()
 		print(np.size(train_data_input,1))
-		model.add(Dense(64, activation = 'relu', input_dim = np.size(train_data_input,1)))
+		model.add(Dense(64, activation = 'relu', input_dim = 11))
+		#model.add(Dense(64, activation = 'relu', input_dim = np.size(train_data_input,1)))
 		model.add(Dense(64, activation = 'relu'))
 		model.add(Dense(1, activation = 'softmax'))
 
 		# Compiling Neural Network
-		model.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+		model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 		# Fitting our model 
-		model.fit(train_data_input, train_data_result, batch_size = 1000, nb_epoch = 100)
+		model.fit(train_data_input, train_data_result, batch_size = 32, nb_epoch = 250)
 
 		model_json = model.to_json()
 		with open(saved_model_name+".json", "w") as json_file:
@@ -184,28 +186,30 @@ def get_pitcher_model(first_name, last_name):
 		print("Loaded model from disk")
 		return loaded_model
 
-	def test_model(data, model, max_val):
+	def test_model(data, data_result, model, max_val):
 		model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-		model_data = data[['prev_pitch_3', 'prev_pitch_2', 'prev_pitch_2', 'balls', 'strikes', 'stand', 'on_3b', 'on_2b', 'on_1b', 'outs_when_up', 'pitch_number']]
-		predict = model.predict(model_data)
-		data['predict_type'] = predict
+		model_data = data
+		print(model_data)
+		predict = model.predict_on_batch(model_data)
+		print("With ",model_data," Predict: ",predict)
+		#data['predict_type'] = predict
 		data *= max_val
-		return data[['pitch_code', 'predict_type']]
+		print("PREDICT",predict)
+		return predict, data_result
 
 
 	#Get the pitch data and process it so that it is ready for the machine learning model
 	train_data_input, train_data_result, test_data = get_data(first_name, last_name)
-	train_data_input, train_data_result, test_data, max_pitch_val = pre_process_data(train_data_input, train_data_result, test_data)
-
+	train_data_input, train_data_result, test_data, test_data_result, max_pitch_val = pre_process_data(train_data_input, train_data_result, test_data)
 	saved_model_name = 'Data/'+str(last_name)+"_"+str(first_name)+"_model"
-
+	print(train_data_input)
 	#If we already have the model, load it, else make it
 	if os.path.isfile(saved_model_name+'.h5'):
 		model = fetch_model(saved_model_name)
 	else:
 		model = create_model(train_data_input, train_data_result, saved_model_name)
 
-	test_data = test_model(test_data, model, max_pitch_val)
+	test_data = test_model(test_data, test_data_result, model, max_pitch_val)
 	print(test_data)
 	return test_data
 
